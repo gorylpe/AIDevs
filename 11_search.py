368038ee917b2f8d62ff86b992c6ad3e9b7c034d
Piotr Przestrzelski <gorylp71@gmail.com>
2024-04-11 09:44:14 +0200

    feat: using fastembed from qdrant to embed all data

--- 11_search.py
+++ 11_search.py
@@ -2,12 +2,43 @@ from AIDevs import AIDevsTasks, API_KEY, OPENAI_API_KEY
 from langchain_openai import ChatOpenAI
 import docker
 from docker.models.containers import Container
-import time
+from qdrant_client import QdrantClient
+import json
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=OPENAI_API_KEY)
-dev_task = AIDevsTasks(API_KEY, "whoami", debug=True)
+dev_task = AIDevsTasks(API_KEY, "search", debug=True)
 docker_client = docker.from_env()
 
-container: Container = docker_client.containers.run("qdrant/qdrant", detach=True)
-time.sleep(5)
-container.remove(force=True)
+port = 6333
+container: Container = None
+try:
+    container = docker_client.containers.run(
+        "qdrant/qdrant", detach=True, name="11_search", ports={f"{port}/tcp": port}
+    )
+except Exception as e:
+    print("Container already created")
+
+qdrant_client = QdrantClient(host="localhost", port=port)
+
+f = open("archiwum_aidevs.json", "r")
+data: list[dict] = json.load(f)
+f.close()
+
+inserted_data_count = qdrant_client.count("docs")
+if inserted_data_count.count < len(data):
+    qdrant_client.delete_collection("docs")
+
+    docs = []
+    metadata = []
+    ids = []
+    for i, d in enumerate(data):
+        docs.append(d["info"])
+        metadata.append({"title": d["title"], "url": d["url"], "date": d["date"]})
+        ids.append(i)
+
+    # Use FastEmbed for embeddings now
+    qdrant_client.add("docs", documents=docs, metadata=metadata, ids=ids)
+else:
+    print("Data already uploaded")
+
+task = dev_task.task()
